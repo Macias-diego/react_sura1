920fd4dbae127fef2eb11b7b5bdbe69ea39e3c63
Diego Macias <diego-macias@hotmail.com>
2022-09-27 17:39:34 -0500

    Se crea App de react

---
+++ src/Componente/Diego.js
@@ -0,0 +1,13 @@
+export function Diego(){
+    
+    let nombre ="Diego"
+    //Interpolar: Controlar la interfaz grafica desde JS
+
+    return(
+        <>
+        <h1>HOLA REACT SOY {nombre}</h1>
+        <hr/>
+        </>
+    )
+
+}
